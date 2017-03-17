a46c4860841869b08b14a152505dbca54f3bf83c
Vincent Ollivier <v@vinc.cc>
2017-03-17 22:10:53 +0100

    Refresh UI before blocking on key derivation in paste#new

--- app/assets/javascripts/pastes.js
+++ app/assets/javascripts/pastes.js
@@ -5,11 +5,44 @@ $(document).on('turbolinks:load', function() {
     $('[name=passphrase]').val(passphrase);
   });
 
-  $('#new_paste').submit(function() {
-    var content = $('[name=content]').val();
+  var form = $('#new_paste');
+
+  $('[name=content]').prop('disabled', false);
+  $('[name=passphrase]').prop('disabled', false);
+
+  form.on('submit', function(e) {
+    // Don't send local data to server
+    $('[name=content]').prop('disabled', true);
+    $('[name=passphrase]').prop('disabled', true);
+
     var passphrase = $('[name=passphrase]').val();
 
     if (passphrase.length > 0) {
+      $('[type=submit]', form).
+        removeData('disable-with').
+        removeAttr('disable-with').
+        prop('disabled', true).
+        val('Derivating key ...');
+    }
+
+    // Derivating a key from the passphrase will block the UI thread,
+    // and we need to refresh the UI before that.
+    // So we need to remove `remote: true` in `from_tag` and handle
+    // the remote call here.
+    setTimeout(function() {
+      $.rails.handleRemote(form);
+    }, 0);
+
+    e.preventDefault();
+  });
+
+  form.on('ajax:before', function() {
+    var content = $('[name=content]').val();
+    var passphrase = $('[name=passphrase]').val();
+
+
+    if (passphrase.length > 0) {
+      // NOTE: The key derivation function will block the UI thread
       var encryptedContent = Crypto.encrypt(content, passphrase);
 
       $('[name="paste[content]"]').val(encryptedContent);
@@ -18,10 +51,7 @@ $(document).on('turbolinks:load', function() {
       $('[name="paste[content]"]').val(content);
       $('[name="paste[encrypted]"]').val(false);
     }
-
-    // Don't send local data to server
-    $('[name=content]').prop('disabled', true);
-    $('[name=passphrase]').prop('disabled', true);
+    $('[type=submit]', form).val('Sending paste ...');
   });
 
   var decryptContent = function(passphrase) {
